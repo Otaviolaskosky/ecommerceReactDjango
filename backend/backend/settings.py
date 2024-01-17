6a814b5c9aa3aada64cf11242438935a6eb65cc9
Otaviolaskosky <nick_ol_@hotmail.com>
2024-01-17 21:40:00 +0000

    v5

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,10 +38,17 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
 
+    'rest_framework',
+    'corsheaders',
+
     'base.apps.BaseConfig',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    # "django.middleware.common.CommonMiddleware",
+
+
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -123,3 +130,18 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     "https://special-waffle-wr6gxvrjjx63g577-8000.app.github.dev",
+#     # Add other domains as needed
+# ]
+# CORS_ALLOW_HEADERS = [
+#     'access-control-allow-headers',
+#     'access-control-allow-origin',
+#     'content-type',
+#     'your-custom-header',
+#     'https://special-waffle-wr6gxvrjjx63g577-8000.app.github.dev'
+# ]
+
+
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,7 +1,56 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .products import products
 
 # Create your views here.
 
+@api_view(['GET'])
 def getRoutes(request):
-    return JsonResponse('Hello', safe=False)
+    routes = [
+        '/api/products/',
+        '/api/products/create/',
+
+        '/api/products/upload/',
+
+        '/api/products/<id>/reviews/',
+
+        '/api/products/top/',
+        '/api/products/<id>/',
+
+        '/api/products/delete/<id>/',
+        '/api/products/<update>/<id>/',
+        
+    ]
+    return Response(routes)
+
+@api_view(['GET'])
+def getProducts(request):
+    return Response(products)
+
+@api_view(['GET'])
+def getProduct(request, pk):
+    product = None
+    for i in products:
+        if i['_id'] == pk:
+            product = i
+            break
+
+    return Response(product)
+
+
+
+
+@require_http_methods(["OPTIONS", "GET"])
+def your_view(request):
+    # Your view logic here
+    response_data = {'message': 'Success'}
+    
+    # If it's a preflight OPTIONS request, return an empty response
+    if request.method == 'OPTIONS':
+        return JsonResponse({}, status=200)
+
+    return JsonResponse(response_data)
