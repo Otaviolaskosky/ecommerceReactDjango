8562a264b179dbefca8c71ffa545694113914ab1
Otaviolaskosky <nick_ol_@hotmail.com>
2024-01-28 21:45:52 +0000

    Aula 20 modulo 4

--- backend/base/views.py
+++ backend/base/views.py
@@ -4,7 +4,9 @@ from django.views.decorators.http import require_http_methods
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
+from .models import Product
 from .products import products
+from .serailizers import ProductSerializer
 
 # Create your views here.
 
@@ -29,17 +31,22 @@ def getRoutes(request):
 
 @api_view(['GET'])
 def getProducts(request):
-    return Response(products)
+    products = Product.objects.all()
+    serailizer = ProductSerializer(products, many=True)
+    return Response(serailizer.data)
 
 @api_view(['GET'])
 def getProduct(request, pk):
-    product = None
-    for i in products:
-        if i['_id'] == pk:
-            product = i
-            break
+    # product = None
+    # for i in products:
+    #     if i['_id'] == pk:
+    #         product = i
+    #         break
+    product = Product.objects.get(_id=pk)
+    serailizer = ProductSerializer(product, many=False)
+    return Response(serailizer.data)
 
-    return Response(product)
+    # return Response(product)
 
 
 
