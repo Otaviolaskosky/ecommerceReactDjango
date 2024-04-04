3c137a0cd513ceb7b9681abf3cf6c8aa500d3bf7
Otaviolaskosky <nick_ol_@hotmail.com>
2024-04-04 19:19:10 +0000

    Class 48

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -5,6 +5,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import FormContainer from "../components/FormContainer"
 import { login } from '../actions/userActions'
 import { saveShippingAddress } from '../actions/cartActions'
+import CheckoutSteps from '../components/CheckoutSteps'
 
 function ShippingScreen({ history }) {
 
@@ -26,6 +27,8 @@ function ShippingScreen({ history }) {
 
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2/>
+      <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
 
       <Form.Group controlId='address'>
